913ae347aa335fee4a2895fdb7b9d150ab9bba43
MelvinMcMonagle <36984767+MelvinMcMonagle@users.noreply.github.com>
2019-12-18 16:59:47 +0100

    Update portal.js

--- 04-integrate-new-frontend-in-communication-portal/portal/src/portal.js
+++ 04-integrate-new-frontend-in-communication-portal/portal/src/portal.js
@@ -6,13 +6,11 @@ import {hashPrefix, loadApp} from './helper';
 async function init() {
     const globalEventDistributor = new GlobalEventDistributor();
     const loadingPromises = [];
-
-    // The URLs /app1 - /app4 are redirected to different ports by the webpack proxy (webpack.config.js)
+    
     loadingPromises.push(loadApp('navBar', null, '/navBar/singleSpaEntry.js', null, null));
     loadingPromises.push(loadApp('reactApp', null, '/reactApp/singleSpaEntry.js', '/reactApp/store.js', globalEventDistributor));
     loadingPromises.push(loadApp('angularApp', null, '/angularApp/singleSpaEntry.js', '/angularApp/store.js', globalEventDistributor));
 
-    // wait until all stores are loaded and all apps are registered with singleSpa
     await Promise.all(loadingPromises);
 
     singleSpa.start();
