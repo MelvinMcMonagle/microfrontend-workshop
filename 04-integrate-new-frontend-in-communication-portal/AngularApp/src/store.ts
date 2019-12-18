850d2b87916cef5b19870ab7a6d7dce8dcb2bf85
MelvinMcMonagle <36984767+MelvinMcMonagle@users.noreply.github.com>
2019-12-18 18:19:25 +0100

    Update store.ts

--- 04-integrate-new-frontend-in-communication-portal/AngularApp/src/store.ts
+++ 04-integrate-new-frontend-in-communication-portal/AngularApp/src/store.ts
@@ -11,21 +11,12 @@ export const INITIAL_STATE: IAppState = {
 
 export class ListActions {
     static FINISH_ITEM = 'FINISH_ITEM';
-    static FINISH_ALL = 'FINISH_ALL';
-    static REMOVE_ALL = 'REMOVE_ALL';
     static ADD_ITEM = 'ADD_ITEM';
 
     finishItem(payload: number): any {
         return {type: ListActions.FINISH_ITEM, payload: payload};
     }
 
-    finishAll(): Action {
-        return {type: ListActions.FINISH_ALL};
-    }
-
-    removeAll(): Action {
-        return {type: ListActions.REMOVE_ALL};
-    }
 }
 
 export function rootReducer(lastState: IAppState, action: any): IAppState {
@@ -37,17 +28,7 @@ export function rootReducer(lastState: IAppState, action: any): IAppState {
         case ListActions.FINISH_ITEM:
             lastState.elementList[action.payload] = {...lastState.elementList[action.payload], completed: true};
             return {elementList: lastState.elementList};
-        case ListActions.FINISH_ALL:
-            return {
-                elementList: lastState.elementList.map(item => {
-                    item.completed = true;
-                    return item;
-                })
-            };
-        case ListActions.REMOVE_ALL:
-            return {
-                elementList: []
-            };
+ 
 
     }
 
