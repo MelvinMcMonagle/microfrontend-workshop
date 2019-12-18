b9afbb57b23956a60433f30aa7408baf79244edf
MelvinMcMonagle <36984767+MelvinMcMonagle@users.noreply.github.com>
2019-12-18 17:00:53 +0100

    Update helper.js

--- 04-integrate-new-frontend-in-communication-portal/portal/src/helper.js
+++ 04-integrate-new-frontend-in-communication-portal/portal/src/helper.js
@@ -1,4 +1,4 @@
-import * as singleSpa from 'single-spa'; // waiting for this to be merged: https://github.com/CanopyTax/single-spa/pull/156
+import * as singleSpa from 'single-spa'; 
 
 export function hashPrefix(prefix) {
     return function (location) {
@@ -9,7 +9,6 @@ export function hashPrefix(prefix) {
 export async function loadApp(name, hash, appURL, storeURL, globalEventDistributor) {
     let storeModule = {}, customProps = {globalEventDistributor: globalEventDistributor};
 
-    // try to import the store module
     try {
         storeModule = storeURL ? await SystemJS.import(storeURL) : {storeInstance: null};
     } catch (e) {
@@ -17,13 +16,13 @@ export async function loadApp(name, hash, appURL, storeURL, globalEventDistribut
     }
 
     if (storeModule.storeInstance && globalEventDistributor) {
-        // add a reference of the store to the customProps
+       
         customProps.store = storeModule.storeInstance;
 
-        // register the store with the globalEventDistributor
+        
         globalEventDistributor.registerStore(storeModule.storeInstance);
     }
 
-    // register the app with singleSPA and pass a reference to the store of the app as well as a reference to the globalEventDistributor
+    
     singleSpa.registerApplication(name, () => SystemJS.import(appURL), hash ? hashPrefix(hash): () => true, customProps);
 }
